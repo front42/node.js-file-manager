3c51dbda8dd4a6606533b8034ada93f6a22f6815
front42 <altaifront@gmail.com>
2024-02-02 21:29:53 +0300

    feat: add up-navigation with new path-resolving strategy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { stdin as input, stdout as output } from 'node:process';
 
 import getHomeDir from './getHomeDir.js';
 import getUserName from './getUserName.js';
+import getDirectoryContent from './getDirectoryContent.js';
+import goUp from './goUp.js';
+
+export const pathObject = { currentPath: getHomeDir() };
 
 console.log(`Welcome to the File Manager, ${getUserName()}!`);
 console.log(`You are currently in ${getHomeDir()}`);
@@ -11,12 +15,19 @@ const rl = readline.createInterface({ input, output });
 
 rl.on('line', value => {
   switch (value) {
+    case 'ls':
+      getDirectoryContent();
+      break;
+    case 'cd ..':
+    case 'up':
+      goUp();
+      break;
     case '.exit':
       rl.close();
       return;
     default:
       console.log('Invalid input');
-      console.log(`You are currently in ${getHomeDir()}`);
+      console.log(`You are currently in ${pathObject.currentPath}`);
   }
 });
 
